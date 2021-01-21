95a2b7897f367e6336e87c18895b234193860b1e
Sean <sean@vigorwebsolutions.com>
2021-01-21 09:59:39 -0800

    Capture & Present

--- src/frontend/ui/index.ts
+++ src/frontend/ui/index.ts
@@ -1,8 +1,10 @@
 import { KojiBridge } from '../bridge';
 import { capture, Capture } from './capture';
+import { present, Present } from './present';
 
 export class UI extends KojiBridge {
   capture: Capture = capture;
+  present: Present = present;
 }
 
 export const ui = new UI();
---
+++ src/frontend/ui/present/index.ts
@@ -0,0 +1,51 @@
+import { KojiBridge } from '../../bridge';
+import { client } from '../../@decorators/client';
+
+export interface PresentConfirmationOptions {
+  title?: string;
+  message?: string;
+  confirmButtonLabel?: string;
+  cancelButtonLabel?: string;
+}
+
+export interface PresentAlertOptions {
+  title?: string;
+  message?: string;
+}
+
+export class Present extends KojiBridge {
+  @client
+  async confirmation(options: PresentConfirmationOptions = {}): Promise<boolean> {
+    const data = await this.sendMessageAndAwaitResponse(
+      {
+        kojiEventName: 'Koji.ConfirmPrompt',
+        data: {
+          title: options.title,
+          message: options.message,
+          confirmButtonLabel: options.confirmButtonLabel,
+          cancelButtonLabel: options.cancelButtonLabel,
+        },
+      },
+      'Koji.CaptureSuccess',
+    );
+
+    return data.didConfirm;
+  }
+
+  @client
+  alert(options: PresentConfirmationOptions = {}): void {
+    this.sendMessage(
+      {
+        kojiEventName: 'Koji.Alert',
+        data: {
+          title: options.title,
+          message: options.message,
+          confirmButtonLabel: options.confirmButtonLabel,
+          cancelButtonLabel: options.cancelButtonLabel,
+        },
+      },
+    );
+  }
+}
+
+export const present = new Present();
