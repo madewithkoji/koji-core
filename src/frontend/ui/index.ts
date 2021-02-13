b28087eb304d0665b650688a28e228f2f2493b8c
Sean <sean@vigorwebsolutions.com>
2021-02-12 17:28:19 -0800

    Add support for navigate

--- src/frontend/ui/index.ts
+++ src/frontend/ui/index.ts
@@ -1,9 +1,11 @@
 import { KojiBridge } from '../kojiBridge';
 import { capture, Capture } from './capture';
+import { navigate, Navigate } from './navigate';
 import { present, Present } from './present';
 
 export class UI extends KojiBridge {
   capture: Capture = capture;
+  navigate: Navigate = navigate;
   present: Present = present;
 }
 
