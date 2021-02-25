089587c47c6af883ef85786b43d2536ad7d433d1
Sean <sean@vigorwebsolutions.com>
2021-02-24 17:43:43 -0800

    Temp fix for decoding overrides

--- src/backend/middleware/index.ts
+++ src/backend/middleware/index.ts
@@ -12,6 +12,18 @@ export interface KojiConfig {
   '@@initialTransform'?: any;
 }
 
+// ToDo: Make this non-mutating
+const decodeObject = (obj: any): any => {
+  Object.keys(obj).forEach((key) => {
+    if (obj[key] && typeof obj[key] === 'object') {
+      decodeObject(obj[key]);
+      return;
+    }
+    // eslint-disable-next-line no-param-reassign
+    obj[key] = decodeURI(obj[key]);
+  });
+};
+
 /**
  * Executes middleware, making koji-specific data available on the res.locals property.
  *
@@ -35,6 +47,10 @@ export function middleware(kojiConfig: KojiConfig = {}) {
     const overrides = req.headers['x-trusted-koji-overrides'];
 
     const parsedOverrides = typeof overrides === 'string' ? JSON.parse(overrides) : {};
+
+    // Overrides coming from the headers are URL-encoded, so we need to decode them
+    // before using them
+    decodeObject(parsedOverrides);
 
     // Pull the remixData property from our overrides (which will include the entire koji.json contents)
     const { remixData: remixDataOverride = {} } = parsedOverrides;
