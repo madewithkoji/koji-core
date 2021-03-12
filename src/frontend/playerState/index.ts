765e255c3bce29a7f2c67366ca65587bf277d026
Sean <sean@vigorwebsolutions.com>
2021-03-11 17:04:54 -0800

    Add hasFocus, onFocus, onBlur

--- src/frontend/playerState/index.ts
+++ src/frontend/playerState/index.ts
@@ -44,13 +44,13 @@ export interface EditorAttributes {
 export type ReceiptType = 'seller' | 'buyer';
 
 export type IsRemixingCallback =
-/**
-  * Function to handle changes in remix state. Receives the `isRemixing` and `editorAttributes` properties as inputs.
-  *
-  * @param isRemixing Indicates whether the Koji is in remixing mode.
-  * @param editorAttributes
-  */
-(isRemixing: boolean, editorAttributes: EditorAttributes) => void;
+  /**
+   * Function to handle changes in remix state. Receives the `isRemixing` and `editorAttributes` properties as inputs.
+   *
+   * @param isRemixing Indicates whether the Koji is in remixing mode.
+   * @param editorAttributes
+   */
+  (isRemixing: boolean, editorAttributes: EditorAttributes) => void;
 
 /**
  * Manages the context of the Koji to enable distinct experiences for different users and views.
@@ -60,24 +60,58 @@ export class PlayerState extends KojiBridge {
   public context: PlayerStateContext = 'default';
   /** The type of receipt. */
   public receiptType?: ReceiptType;
+  /** Focus state of the Koji. */
+  public hasFocus: boolean = false;
 
   public constructor() {
     super();
 
     // ToDo: Make this better, as it's just a way to get around the isomorphism
     // of this package
-    if (typeof window as any === 'undefined') return;
+    if ((typeof window as any) === 'undefined') return;
 
+    // Pull off any query parameters
     const params: ExpectedQueryParameters = qs.parse(window.location.search, { ignoreQueryPrefix: true });
 
+    // First, look for the screenshot context
     if (window.location.href.includes('koji-screenshot')) {
       this.context = 'screenshot';
     } else {
+      // Otherwise, pull the context from the query parameters
       const { context = 'default', 'dynamic-receipt': receiptType } = params;
 
       this.context = context;
       this.receiptType = receiptType;
     }
+
+    // Set the initial value based on the feed hash
+    this.hasFocus = !window.location.hash.includes('#koji-feed-key=');
+  }
+
+  /**
+   * Listens to when a Koji is returned to a focus state.
+   *
+   * @param   callback  Callback function.
+   * @return            Function to unsubscribe from the focus state listener.
+   */
+  @client
+  public onFocus(callback: Function): Function {
+    return this.execCallbackOnMessage(() => {
+      callback();
+    }, 'KojiFeed.Play');
+  }
+
+  /**
+   * Listens to when a Koji leaves a focus state.
+   *
+   * @param   callback  Callback function.
+   * @return            Function to unsubscribe from the un-focus state listener.
+   */
+  @client
+  public onBlur(callback: Function): Function {
+    return this.execCallbackOnMessage(() => {
+      callback();
+    }, 'KojiFeed.Pause');
   }
 
   /**
@@ -94,12 +128,9 @@ export class PlayerState extends KojiBridge {
    */
   @client
   public subscribe(callback: IsRemixingCallback): Function {
-    return this.execCallbackOnMessage(
-      ({ isRemixing, editorAttributes }: { isRemixing: boolean; editorAttributes: EditorAttributes }) => {
-        callback(isRemixing, editorAttributes);
-      },
-      'KojiPreview.IsRemixing',
-    );
+    return this.execCallbackOnMessage(({ isRemixing, editorAttributes }: { isRemixing: boolean; editorAttributes: EditorAttributes }) => {
+      callback(isRemixing, editorAttributes);
+    }, 'KojiPreview.IsRemixing');
   }
 }
 
