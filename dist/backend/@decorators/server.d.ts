89ac17d98650f449a634cf53e7769c0e52fc1297
Sean <sean@vigorwebsolutions.com>
2021-03-16 17:26:16 -0700

    Remove dist

--- dist/backend/@decorators/server.d.ts
+++
@@ -1,8 +0,0 @@
-/**
- * Verifies that server-side methods are used only in backend services of the Koji.
- * @param   target      Class to which the method belongs.
- * @param   propertyKey Method name.
- * @param   descriptor  Method's behavior (which can be mutated inside this function).
- * @return              Method's behavior or an error, if the method is being invoked in a frontend environment.
- */
-export declare function server(target: Object, propertyKey: string, descriptor: PropertyDescriptor): any;
