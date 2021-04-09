528e0bb5a542a98e9cf65240e2073bf4772f64ae
Sean Thielen <sean@gometa.io>
2021-04-09 15:48:01 -0700

    Passthrough upload for media generated as part of template experience

--- src/frontend/ui/index.ts
+++ src/frontend/ui/index.ts
@@ -2,11 +2,13 @@ import { KojiBridge } from '../kojiBridge';
 import { capture, Capture } from './capture';
 import { navigate, Navigate } from './navigate';
 import { present, Present } from './present';
+import { upload, Upload } from './upload';
 
 export class UI extends KojiBridge {
   capture: Capture = capture;
   navigate: Navigate = navigate;
   present: Present = present;
+  upload: Upload = upload;
 }
 
 export const ui = new UI();
