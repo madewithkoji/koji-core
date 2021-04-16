b435587d0da05dbc8002d519fff713f6694e94e6
Sean <sean@vigorwebsolutions.com>
2021-04-16 11:40:26 -0700

    More interface breakdowns

--- src/frontend/ui/upload/index.ts
+++ src/frontend/ui/upload/index.ts
@@ -1,21 +1,27 @@
 import { KojiBridge } from '../../kojiBridge';
 import { client } from '../../@decorators/client';
 
+/** Specifies the cropping constraints when remuxing a video. If not specified, the video will not be cropped. */
+export interface RemuxPreset {
+  /** The desired aspect ratio */
+  aspectRatio: '16:9'|'9:16'|'4:5'|'1:1'|'passthrough',
+  /** How the video will be cropped */
+  sizePolicy: 'fill'|'fit',
+}
+
+/** Applies a watermark to the uploaded image. Available only with HLS transcoding. */
+export interface Watermark {
+  /** Type of the watermark. `creatorProfileUrl` watermarks the image with `koji.to/@creatorUsername`. */
+  type: 'creatorProfileUrl',
+}
+
 export interface VideoOptions {
   /** Enables HTTP Live Streaming (HLS) for delivery of longer content. When enabled, uploaded videos are transcoded for HLS and saved as an m3u8 playlist. Use this feature in conjunction with [[https://github.com/video-dev/hls.js/ | hls.js]] for controlling playback. */
   hls?: boolean;
   /** Remuxes video files constructed from getUserMedia MediaStreams, which ensures these files contain correct duration headers before they are delivered. */
   remux?: boolean;
-  /** Specifies the cropping constraints when remuxing a video. If not specified, the video will not be cropped. */
-  remuxPreset?: {
-    aspectRatio: '16:9'|'9:16'|'4:5'|'1:1'|'passthrough',
-    sizePolicy: 'fill'|'fit',
-  };
-  /** Applies a watermark to the uploaded image. Available only with HLS transcoding. */
-  watermark?: {
-    /** Type of the watermark. `creatorProfileUrl` watermarks the image with `koji.to/@creatorUsername`. */
-    type: 'creatorProfileUrl',
-  };
+  remuxPreset?: RemuxPreset;
+  watermark?: Watermark;
 }
 
 /**
