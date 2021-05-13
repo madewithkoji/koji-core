c64b234123a3b7a8b711d17e62da67e904b3ea61
rasienko <rachel.sienko@gmail.com>
2021-05-13 09:06:12 -0700

    Add cents for purchase amount

--- src/frontend/iap/index.ts
+++ src/frontend/iap/index.ts
@@ -6,7 +6,7 @@ import { IAPToken } from '../../types';
  * Optional information to add to a {@doclink core-backend-iap#IapReceipt | transaction receipt} for a given in-app purchase.
  */
 export interface PurchaseOptions {
-  /** Amount of the purchase. */
+  /** Amount of the purchase, in cents. */
   amount?: number;
   /** Custom message associated with the purchase. This value is stored as a custom attribute on the transaction receipt. */
   customMessage?: string;
