b9df69ec91190e9e4f30e16213a84428045985c8
sean <admin@koji-projects.com>
2022-01-28 10:45:59 -0800

    Add support for Polygon, batch ERC1155 balance queries

--- src/backend/web3/providers/ethereum.ts
+++ src/backend/web3/providers/ethereum.ts
@@ -8,6 +8,12 @@ import { Base, BackendConfigurationInput } from '../../base';
 enum EthereumRoutes {
   ERC721_GET_BALANCE = '/v1/apps/hooks/web3/erc721/balanceOf',
   ERC1155_GET_BALANCE = '/v1/apps/hooks/web3/erc1155/balanceOf',
+  ERC1155_GET_BALANCE_BATCH = '/v1/apps/hooks/web3/erc1155/balanceOfBatch',
+}
+
+export enum Chain {
+  ETHEREUM = 'eth',
+  POLYGON = 'polygon',
 }
 
 /**
@@ -44,6 +50,7 @@ export class EthereumProvider extends Base {
    *
    * @param   contractId      The ID of the smart contract
    * @param   accountAddress  The address of the account to query
+   * @param chain             Optional chain override. If not set, defaults to Ethereum
    *
    * @example
    * ```javascript
@@ -54,13 +61,18 @@ export class EthereumProvider extends Base {
    * ```
    */
   @server
-  public async getERC721Balance(contractId: string, accountAddress: string): Promise<number> {
+  public async getERC721Balance(
+    contractId: string,
+    accountAddress: string,
+    chain: Chain = Chain.ETHEREUM,
+  ): Promise<number> {
     try {
       const { data } = await axios.post(
         `${this.rootPath}${EthereumRoutes.ERC721_GET_BALANCE}`,
         {
           contractId,
           accountAddress,
+          chain,
         },
         {
           headers: this.rootHeaders,
@@ -78,6 +90,7 @@ export class EthereumProvider extends Base {
    * @param   contractId      The ID of the smart contract
    * @param   accountAddress  The address of the account to query
    * @param   tokenId         The ID of the specific token for which to return a balance
+   * @param chain             Optional chain override. If not set, defaults to Ethereum
    *
    * @example
    * ```javascript
@@ -93,6 +106,7 @@ export class EthereumProvider extends Base {
     contractId: string,
     accountAddress: string,
     tokenId: string,
+    chain: Chain = Chain.ETHEREUM,
   ): Promise<number> {
     try {
       const { data } = await axios.post(
@@ -101,6 +115,7 @@ export class EthereumProvider extends Base {
           contractId,
           accountAddress,
           tokenId,
+          chain,
         },
         {
           headers: this.rootHeaders,
@@ -109,6 +124,53 @@ export class EthereumProvider extends Base {
       return data.balance;
     } catch (err) {
       return 0;
+    }
+  }
+
+  /**
+   * Get an account's balance for multiple ERC1155 token IDs.
+   *
+   * @param contractId      The ID of the smart contract
+   * @param accountAddress  The address of the account to query
+   * @param tokenIds        An array of token IDs
+   * @param chain           Optional chain override. If not set, defaults to Ethereum
+   *
+   * @example
+   * ```javascript
+   * const balances = await ethereum.getERC1155BalanceBatch(
+   *    '0x495f947276749ce646f68ac8c248420045cb7b5e',
+   *    '0x947e4b6e3c2383827fb1a1ace9b191f669089979',
+   *    [
+   *      '13585698947536714042189813736898920110951234445351156220539562030339903717377',
+   *      '13585698947536714042189813736898920110951234445351156220539561952274578145281',
+   *      '91545914907022949088754081908633048759965252325794344822483278837725871996929',
+   *    ],
+   * );
+   * ```
+   */
+  @server
+  public async getERC1155BalanceBatch(
+    contractId: string,
+    accountAddress: string,
+    tokenIds: string[],
+    chain: Chain = Chain.ETHEREUM,
+  ): Promise<{[tokenId: string]: number}> {
+    try {
+      const { data } = await axios.post(
+        `${this.rootPath}${EthereumRoutes.ERC1155_GET_BALANCE_BATCH}`,
+        {
+          contractId,
+          accountAddress,
+          tokenIds,
+          chain,
+        },
+        {
+          headers: this.rootHeaders,
+        },
+      );
+      return data.balances;
+    } catch (err) {
+      return {};
     }
   }
 }
