0d191ea0f5ad52b3b7aef77e8dfb9d6352269b6b
Sean Thielen <sean@gometa.io>
2022-05-22 17:34:33 -0400

    Merge updated custom-vcc-sdk into koji-core (#64)
    * wip. vcc custom control module
    
    * wip. fixes and appId support
    
    * fix
    
    * leave appId in typeoptions
    
    * add cancel event for custom controls
    
    Co-authored-by: Victor Demin <demmbox@gmail.com>

--- src/frontend/ui/capture/index.ts
+++ src/frontend/ui/capture/index.ts
@@ -108,9 +108,11 @@ export interface ExtendedLinkResult {
  * Configuration options for a [[custom]] capture.
  */
 export interface CaptureCustomOptions {
-  /** Short name for the custom control. */
+  /** App ID of the custom control. */
+  appId?: string;
+  /** Short name for the custom control. (to be depreciated) */
   name?: string;
-  /** URL where the custom control is hosted. */
+  /** URL where the custom control is hosted. (to be depreciated) */
   url?: string;
   /** Type options specific to the custom control. */
   typeOptions?: any;
@@ -411,14 +413,15 @@ export class Capture extends KojiBridge {
   @client
   public async custom(options: CaptureCustomOptions = {}, verbose: boolean = false): Promise<any | VerboseCapture> {
     const { name, url, ...typeOptions } = options;
+    const { appId } = options;
 
-    if (!name && !url) throw new Error('Please supply the custom name or url for the Custom VCC you would like to load.');
+    if (!name && !url && !appId) throw new Error('Please supply the custom name or url or appId for the Custom VCC you would like to load.');
 
     const data: CaptureMessage<string> = await this.sendMessageAndAwaitResponse(
       {
         kojiEventName: 'Koji.Capture',
         data: {
-          type: `custom<${name || url}>`,
+          type: `custom<${name || url || appId}>`,
           options: typeOptions,
         },
       },
---
+++ src/frontend/vcc/index.ts
@@ -0,0 +1,82 @@
+import { KojiBridge } from '../kojiBridge';
+import { client } from '../@decorators/client';
+import { CaptureCustomOptions } from '../ui/capture';
+
+export type SetPayload = any;
+
+/**
+ * Module for creating VCC Custom Controls
+ */
+export class VCC extends KojiBridge {
+  /**
+   * Gets the options that are passed to a custom control
+   *
+   * @return Object containing the options
+   *
+   * @example
+   * ```javascript
+   * const captureCustomOptions = await Koji.vcc.getOptions();
+   * ```
+   */
+  @client
+  public async getOptions(): Promise<CaptureCustomOptions> {
+    const data = await this.sendMessageAndAwaitResponse(
+      {
+        kojiEventName: '@@koji/vcc/inbound/getOptions',
+      },
+      '@@koji/vcc/outbound/getOptions',
+    );
+
+    return data.options;
+  }
+
+  /**
+   * Changes the value of a custom control
+   *
+   * @example
+   * ```javascript
+   * Koji.vcc.set("ANY VALUE")
+   * ```
+   */
+  @client
+  public set(payload: SetPayload) {
+    this.sendMessage({
+      kojiEventName: '@@koji/vcc/inbound/set',
+      data: {
+        payload,
+      },
+    });
+  }
+
+  /**
+   * Commits the value of a custom control
+   *
+   * @example
+   * ```javascript
+   * Koji.vcc.finish()
+   * ```
+   */
+  @client
+  public finish() {
+    this.sendMessage({
+      kojiEventName: '@@koji/vcc/inbound/finish',
+    });
+  }
+
+  /**
+   * Dismisses a custom control without saving the value
+   *
+   * @example
+   * ```javascript
+   * Koji.vcc.cancel()
+   * ```
+   */
+  @client
+  public cancel() {
+    this.sendMessage({
+      kojiEventName: '@@koji/vcc/inbound/cancel',
+    });
+  }
+}
+
+export const vcc = new VCC();
